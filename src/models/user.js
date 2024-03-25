a38f63c860717fedef11a25827066bf048aa9044
hakan <tasbeyhakan@gmail.com>
2024-03-25 21:36:01 +0300

    feat: added user model

--- src/models/user.js
+++ src/models/user.js
@@ -4,3 +4,49 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+// User Model:
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set: passwordEncrypt
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+
+
+},{
+    collection: 'users',
+    timestamps: true
+})
+
+// Model:
+module.exports = mongoose.model('User', UserSchema)
+
